7a03363b7c1381de7ec281ada1af2e533007cc20
diegotosato <tosato1998@gmail.com>
2025-12-11 11:49:57 +0100

    use NavLink for navigation and improve structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,54 @@
+import { Link, NavLink, Routes } from "react-router-dom"
+import { BrowserRouter } from "react-router-dom"
+
 function App() {
   return (
     <>
-      <header>
-        Header
-        <nav>
-          <ul>
-            <li>
-              <a href="#">Home</a>
-            </li>
-            <li>
-              <a href="#">Details</a>
-            </li>
-          </ul>
-        </nav>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <header>
+            Header
+            <nav>
+              <ul>
+                <NavLink to="/">
+                  Home
+                </NavLink>
+                <NavLink to="/details">
+                  Details
+                </NavLink>
+              </ul>
+            </nav>
+          </header>
 
-      <main>Main</main>
+          <main>Main</main>
 
-      <footer>
-        Footer
-        <div className="contacts">
-          Contatti
-          <ul>
-            <li>
-              <a href="#">Chi siamo</a>
-              <a href="#">La nostra mission</a>
-              <a href="#">Team</a>
-              <a href="#">Lavora con noi</a>
-              <a href="#">Storia</a>
-            </li>
-          </ul>
-        </div>
-        <div className="support">
-          Supporto
-          <ul>
-            <li>
-              <a href="#">Supporto e assistenza</a>
-              <a href="#">FAQ</a>
-              <a href="#">Assistenza clienti</a>
-            </li>
-          </ul>
-        </div>
-      </footer>
+          <footer>
+            Footer
+            <div className="contacts">
+              Contatti
+              <ul>
+                <li>
+                  <a href="#">Chi siamo</a>
+                  <a href="#">La nostra mission</a>
+                  <a href="#">Team</a>
+                  <a href="#">Lavora con noi</a>
+                  <a href="#">Storia</a>
+                </li>
+              </ul>
+            </div>
+            <div className="support">
+              Supporto
+              <ul>
+                <li>
+                  <a href="#">Supporto e assistenza</a>
+                  <a href="#">FAQ</a>
+                  <a href="#">Assistenza clienti</a>
+                </li>
+              </ul>
+            </div>
+          </footer>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
