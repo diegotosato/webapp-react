8b619f291a8882afc9978246c9c9a0aa06adf694
diegotosato <tosato1998@gmail.com>
2025-12-11 16:09:54 +0100

    fetch movie details and reviews dynamically using movie ID; enhance layout for movie and reviews display

--- src/Pages/Details.jsx
+++ src/Pages/Details.jsx
@@ -4,35 +4,67 @@ import { useParams } from "react-router-dom"
 
 export default function Details() {
 
-    const movieId = useParams()
-    console.log(movieId);
+    const { id } = useParams()
 
 
     //empty array to inglobe movies from DB
-    const [movies, setMovies] = useState([])
+    const [movie, setMovie] = useState([])
+    const [reviews, setReviews] = useState([])
 
     //ajax request at component charge
     useEffect(() => {
-        axios.get(`http://localhost:3000/api/movies/1`)
+        axios.get(`http://localhost:3000/api/movies/${id}`)
             .then(response => {
-                setMovies(response.data)
+                setMovie(response.data)
+                setReviews(response.data.reviews)
             }).catch(err => {
                 console.log(err.message);
             })
     }, [])
-    console.log(movies);
+    console.log(reviews);
 
 
     return (
         <>
             <main>
-                <div className="container">
-                    <div className="row row-cols-1 row-cols-sm-2 row-cols-lg-3 g-3">
+                <div className="container ">
 
-                        <p>ID prodotto: {movieId.id}</p>
+                    <div className="card mb-3">
+                        <div className="row">
+                            <div className="col-md-5">
+                                <img src={`https://placehold.co/200x200?text=${movie.title}`} className="img-fluid rounded-start w-100" alt={movie.title} />
+                            </div>
+                            <div className="col-md-7">
+                                <div className="card-body">
+                                    <h5 className="card-title">{movie.title}</h5>
+                                    <p className="card-text">{movie.abstract}</p>
+                                    <p className="card-text">Director: {movie.director}</p>
+                                    <p className="card-text">Genre: {movie.genre}</p>
+                                    <p className="card-text">Release Year: {movie.release_year}</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="reviews row">
+                        {
+                            reviews.map(review => (
+                                <div className="col">
+                                    <div className={`card ${review.vote > 3 ? 'border-success' : 'border-warning'}`} key={review.id}>
+                                        <div className={`card-header ${review.vote > 3 ? 'text-bg-success' : 'text-bg-warning'}`}>{review.name}</div>
+                                        <div className='card-body'>
+                                            <h5 className="card-title">Vote: {review.vote}</h5>
+                                            <p className="card-text">{review.text}</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            ))
+                        }
 
                     </div>
+
                 </div>
+
             </main>
         </>
     )
