7808b82fa8e3f62cc3dbb44b0e4580d4e08093ef
diegotosato <tosato1998@gmail.com>
2025-12-12 21:04:51 +0100

    remove AdminTable component and integrate its functionality directly into AdminDevPage for better cohesion

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,9 +1,34 @@
-import NavBar from "./NavBar"
+import { Link, NavLink } from "react-router-dom"
+
 
 export default function Header() {
     return (
         <>
-            <NavBar />
+            <nav className="navbar navbar-expand-sm navbar-light bg-light px-5 mb-5">
+                <a className="navbar-brand" href="#">MovieAPI</a>
+                <button className="navbar-toggler d-lg-none"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#collapsibleNavId"
+                    aria-controls="collapsibleNavId"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="collapsibleNavId">
+                    <ul className="navbar-nav me-auto mt-2 mt-lg-0">
+                        <li className="nav-item">
+                            <NavLink className="text-decoration-none me-3" to="/">
+                                Home
+                            </NavLink>
+                            <NavLink className="text-decoration-none" to="/admin/login">
+                                Admin
+                            </NavLink>
+                        </li>
+                    </ul>
+
+                </div>
+            </nav>
         </>
     )
 }
--- src/Pages/AdminDevPage.jsx
+++ src/Pages/AdminDevPage.jsx
@@ -1,6 +1,5 @@
 import axios from "axios"
 import { useState, useEffect } from "react"
-import AdminTable from "../Components/Admin/AdminTable"
 import AdminAddMovieForm from "../Components/Admin/AdminAddMovieForm"
 
 export default function AdminDevPage() {
@@ -43,7 +42,51 @@ export default function AdminDevPage() {
                     (show ? <AdminAddMovieForm movies={movies} setMovies={setMovies} setShow={setShow} /> : '')
                 }
 
-                <AdminTable movies={movies} handleAddForm={handleAddForm} />
+                <div className="table-responsive">
+
+                    <table className="table table-dark table-striped align-middle">
+                        <thead className="align-middle">
+                            <tr>
+                                <th>Title</th>
+                                <th>Image</th>
+                                <th>Director</th>
+                                <th>Genre</th>
+                                <th>Release Year</th>
+                                <th>Abstract</th>
+                                <th className="text-center">
+                                    <button className="btn btn-sm btn-outline-light" onClick={handleAddForm}>
+                                        <i className="bi bi-plus-circle"></i> Add
+                                    </button>
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                movies.map(movie => (
+
+                                    <tr className="" key={movie.id}>
+                                        <td>{movie.title}</td>
+                                        <td>
+                                            <img src={`../../public/movies_cover/${movie.image}`} alt={movie.title}
+                                                style={{ height: 100, overflow: "hidden" }} className="border border-white" />
+                                        </td>
+                                        <td>{movie.director}</td>
+                                        <td>{movie.genre}</td>
+                                        <td>{movie.release_year}</td>
+                                        <td>{movie.abstract}</td>
+                                        <td className="text-center">
+                                            <div className="d-flex justify-content-center gap-3">
+                                                <button className="btn btn-sm btn-outline-warning"><i className="bi bi-pencil"></i></button>
+                                                <button className="btn btn-sm btn-outline-danger"><i className="bi bi-trash3"></i></button>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                ))
+                            }
+                        </tbody>
+                    </table>
+
+                </div>
 
             </div>
 
