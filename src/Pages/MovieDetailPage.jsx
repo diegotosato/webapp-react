cdf415c932c4450b3cc38d4dfbe1c17b85e58489
diegotosato <tosato1998@gmail.com>
2025-12-12 13:05:52 +0100

    add review form to MovieDetailPage with name, rating, and review fields

--- src/Pages/MovieDetailPage.jsx
+++ src/Pages/MovieDetailPage.jsx
@@ -51,6 +51,47 @@ export default function MovieDetailPage() {
 
                 </div>
 
+                <hr className="my-5" />
+
+                <div className="container">
+
+                    <h3 className="mb-3">Leave your review</h3>
+
+                    <form>
+                        <div className="row row-cols-2">
+
+                            <div className="mb-3">
+                                <label htmlFor="name" className="form-label">Name</label>
+                                <input type="text" className="form-control" name="name" id="name" aria-describedby="nameHelper" placeholder="Your name here" />
+                                <small id="nameHelper" className="form-text text-muted">Insert your name here</small>
+                            </div>
+
+                            <div className="mb-3">
+                                <label for="rating" className="form-label">Rating</label>
+                                <select className="form-select" name="rating" id="rating"                            >
+                                    <option value="">1</option>
+                                    <option value="">2</option>
+                                    <option selected value="">3</option>
+                                    <option value="">4</option>
+                                    <option value="">5</option>
+                                </select>
+                            </div>
+
+                        </div>
+
+
+                        <div className="mb-3">
+                            <label htmlFor="review" className="form-label">Review</label>
+                            <textarea className="form-control" name="review" id="review" rows="2" placeholder="Your review here..." style={{ resize: "none" }}></textarea>
+                        </div>
+                        <button type="submit" className="btn btn-primary">
+                            <i class="bi bi-send"></i> Send
+                        </button>
+
+                    </form>
+
+                </div>
+
             </main>
         </>
     )
