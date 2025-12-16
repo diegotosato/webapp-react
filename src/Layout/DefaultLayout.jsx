06d7817a3056c60c24a1e8243672b25f87c78f01
diegotosato <tosato1998@gmail.com>
2025-12-16 12:09:46 +0100

    refactor: remove Loader component from HomePage and integrate it into DefaultLayout for better loading management

--- src/Layout/DefaultLayout.jsx
+++ src/Layout/DefaultLayout.jsx
@@ -2,9 +2,20 @@ import Header from "../Components/Header"
 import Footer from "../Components/Footer"
 import { Outlet } from "react-router-dom"
 
+import Loader from "../Components/Loader"
+
+import { useCustom } from "../contexts/CustomContexts"
+
 export default function DefaultLayout() {
+
+    //import from custom context
+    const { loading } = useCustom()
+
     return (
         <>
+            {
+                !loading && <Loader />
+            }
             <Header />
             <Outlet />
             <Footer />
--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,14 +1,13 @@
 import axios from "axios"
 import { useState, useEffect } from "react"
 import MovieListCard from "../Components/MovieListCard"
-import Loader from "../Components/Loader"
 
 import { useCustom } from "../contexts/CustomContexts"
 
 export default function HomePage() {
 
     //import from custom context
-    const { loading, setLoading } = useCustom()
+    const { setLoading } = useCustom()
 
     //empty array to inglobe movies from DB
     const [movies, setMovies] = useState([])
@@ -31,9 +30,7 @@ export default function HomePage() {
     return (
         <>
             <main>
-                {
-                    !loading && <Loader />
-                }
+
                 <div className="container">
                     <div className="row row-cols-1 row-cols-sm-2 row-cols-lg-3 g-5">
                         {
